5cb391602824c98ed5d26b8f67184c5885139144
Artiom Iurcovschi <artiom.08@gmail.com>
2023-11-30 20:34:00 +0200

    initial commit

---
+++ src/main/kotlin/com/exercise/political/speech/PoliticalSpeechApplication.kt
@@ -0,0 +1,11 @@
+package com.exercise.political.speech
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PoliticalSpeechApplication
+
+fun main(args: Array<String>) {
+	runApplication<PoliticalSpeechApplication>(*args)
+}
