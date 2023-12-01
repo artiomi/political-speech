256f513218053beb48fe10ddf878abdd53202d15
Artiom Iurcovschi <artiom.08@gmail.com>
2023-12-01 18:30:31 +0200

    update Id field type

--- src/main/kotlin/com/exercise/political/speech/repositories/PoliticalSpeechRepo.kt
+++ src/main/kotlin/com/exercise/political/speech/repositories/PoliticalSpeechRepo.kt
@@ -3,4 +3,4 @@ package com.exercise.political.speech.repositories
 import com.exercise.political.speech.models.PoliticalSpeech
 import org.springframework.data.repository.CrudRepository
 
-interface PoliticalSpeechRepo : CrudRepository<PoliticalSpeech, Long>
+interface PoliticalSpeechRepo : CrudRepository<PoliticalSpeech, String?>
